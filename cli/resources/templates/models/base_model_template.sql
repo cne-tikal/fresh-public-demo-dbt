e86b3beb6aa831c8dcb6e16f0a09e873c200f0e3
tikadm <20375319+shellegtk@users.noreply.github.com>
2025-09-04 19:51:21 +0300

    Copy cli/resources/templates/models/base_model_template.sql from template cne-dbt-template

---
+++ cli/resources/templates/models/base_model_template.sql
@@ -0,0 +1,15 @@
+
+
+WITH source AS (
+
+    SELECT * FROM {{ source('<SOURCE_NAME>', '<TABLE_NAME>') }}
+
+), <MODEL_NAME> AS (
+
+    SELECT <COLUMN_LIST>
+    FROM source
+
+)
+
+SELECT * 
+FROM <MODEL_NAME>
